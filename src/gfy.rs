f03331e723a523559530577a77372d1ed4aa484e
Jake Marsden <jakemarsdenjm@gmail.com>
2020-06-24 16:15:39 +1200

    Parse additional properties from `/gfycat/{gfyid}`

--- src/gfy.rs
+++ src/gfy.rs
@@ -1,9 +1,12 @@
+use chrono::serde::ts_seconds;
+use chrono::{DateTime, Utc};
 use num_derive::{FromPrimitive, ToPrimitive};
 use num_traits::{FromPrimitive, ToPrimitive};
 use serde::de::Visitor;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use std::cmp::PartialEq;
+use std::collections::HashMap;
 use std::fmt::{Debug, Display, Formatter};
 
 #[derive(Clone, Debug)]
@@ -13,16 +16,49 @@ pub struct Client {
     api_version: u32,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Item {
+    avg_color: String,
+    #[serde(rename = "content_urls")]
+    content_urls: HashMap<String, ItemContent>,
+    #[serde(with = "ts_seconds")]
+    create_date: DateTime<Utc>,
     gfy_id: String,
     gfy_name: String,
+    #[serde(with = "wrapped_with_str")]
+    gfy_number: u64,
+    gfy_slug: String,
+    has_audio: bool,
+    has_transparency: bool,
+    height: u32,
     language_categories: Vec<String>,
+    md5: String,
     nsfw: Nsfw,
     published: Published,
     tags: Vec<String>,
     title: String,
+    user_data: User,
+    width: u32,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ItemContent {
+    height: u32,
+    size: u64,
+    url: String, // TODO: store as a url::Url ?
+    width: u32,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    name: String,
+    profile_image_url: String, // TODO: store as a url::Url ?
+    url: String,               // TODO: store as a url::Url ?
+    username: String,
+    verified: bool,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -45,8 +81,6 @@ pub enum Nsfw {
     Adult = 1,
     PotentiallyOffensive = 3,
 }
-
-struct NsfwVisitor;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -141,27 +175,56 @@ impl<'de> Deserialize<'de> for Nsfw {
     where
         D: Deserializer<'de>,
     {
+        struct NsfwVisitor;
+
+        impl<'de> Visitor<'de> for NsfwVisitor {
+            type Value = Nsfw;
+
+            fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                formatter.write_str("a stringify-ed integer")
+            }
+
+            fn visit_str<E>(self, value: &str) -> std::result::Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                use serde::de::Error;
+
+                let u8_value = value.parse::<u8>().map_err(Error::custom)?;
+                Self::Value::from_u8(u8_value)
+                    // TODO: nice error message
+                    .ok_or_else(|| Error::custom("TODO: nice error message"))
+            }
+        }
+
         deserializer.deserialize_str(NsfwVisitor)
     }
 }
 
-impl<'de> Visitor<'de> for NsfwVisitor {
-    type Value = Nsfw;
+mod wrapped_with_str {
+    use serde::de::Error;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+    use std::fmt::Display;
+    use std::result::Result;
+    use std::str::FromStr;
 
-    fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
-        formatter.write_str("a stringify-ed integer")
+    pub fn serialize<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        T: ToString,
+        S: Serializer,
+    {
+        let str = value.to_string();
+        String::serialize(&str, serializer)
     }
 
-    fn visit_str<E>(self, value: &str) -> std::result::Result<Self::Value, E>
+    pub fn deserialize<'de, T, D>(deserializer: D) -> Result<T, D::Error>
     where
-        E: serde::de::Error,
+        T: FromStr,
+        T::Err: Display,
+        D: Deserializer<'de>,
     {
-        // TODO: `unimplemented!()` is not error handling!
-        //   e.g.: `E::custom(format!("{}", value))`
-        let u8_value = value.parse::<u8>().map_err(|_err| unimplemented!())?;
-        // TODO: `unimplemented!()` is not error handling!
-        //   e.g.: `E::custom(format!("{}", value))`
-        Self::Value::from_u8(u8_value).ok_or_else(|| unimplemented!())
+        let str = String::deserialize(deserializer)?;
+        T::from_str(&str).map_err(Error::custom)
     }
 }
 
@@ -184,25 +247,114 @@ impl Display for Error {
 
 #[cfg(test)]
 mod tests {
-    use super::{Client, Error, Nsfw, Published};
+    use super::{Client, Error, Item, ItemContent, Nsfw, Published, User};
+    use chrono::{TimeZone, Utc};
+    use maplit::hashmap;
 
     #[tokio::test]
     async fn my_test() -> std::result::Result<(), Error> {
+        let to_string_vec = |vec: &[&str]| {
+            vec.iter()
+                .map(|str| String::from(*str))
+                .collect::<Vec<String>>()
+        };
+        let expected_content_urls = hashmap! {
+            String::from("100pxGif") => ItemContent {
+                height: 217,
+                size: 870_224,
+                url: String::from("https://thumbs.gfycat.com/EnormousDescriptiveIndri-max-1mb.gif"),
+                width: 340,
+            },
+            String::from("largeGif") => ItemContent {
+                height: 383,
+                size: 3_675_771,
+                url: String::from("https://thumbs.gfycat.com/EnormousDescriptiveIndri-size_restricted.gif"),
+                width: 600,
+            },
+            String::from("max1mbGif") => ItemContent {
+                height: 217,
+                size: 870_224,
+                url: String::from("https://thumbs.gfycat.com/EnormousDescriptiveIndri-max-1mb.gif"),
+                width: 340,
+            },
+            String::from("max2mbGif") => ItemContent {
+                height: 306,
+                size: 1_698_034,
+                url: String::from("https://thumbs.gfycat.com/EnormousDescriptiveIndri-small.gif"),
+                width: 480,
+            },
+            String::from("max5mbGif") => ItemContent {
+                height: 383,
+                size: 3_675_771,
+                url: String::from("https://thumbs.gfycat.com/EnormousDescriptiveIndri-size_restricted.gif"),
+                width: 600,
+            },
+            String::from("mobile") => ItemContent {
+                height: 408,
+                size: 64_812,
+                url: String::from("https://thumbs.gfycat.com/EnormousDescriptiveIndri-mobile.mp4"),
+                width: 640,
+            },
+            String::from("mobilePoster") => ItemContent {
+                height: 408,
+                size: 17_164,
+                url: String::from("https://thumbs.gfycat.com/EnormousDescriptiveIndri-mobile.jpg"),
+                width: 640,
+            },
+            String::from("mp4") => ItemContent {
+                height: 600,
+                size: 652_935,
+                url: String::from("https://giant.gfycat.com/EnormousDescriptiveIndri.mp4"),
+                width: 940,
+            },
+            String::from("webm") => ItemContent {
+                height: 600,
+                size: 96_156,
+                url: String::from("https://giant.gfycat.com/EnormousDescriptiveIndri.webm"),
+                width: 940,
+            },
+            String::from("webp") => ItemContent {
+                height: 0,
+                size: 131_840,
+                url: String::from("https://thumbs.gfycat.com/EnormousDescriptiveIndri.webp"),
+                width: 0,
+            },
+        };
+        let expected_item = Item {
+            avg_color: String::from("#B99A65"),
+            content_urls: expected_content_urls.clone(),
+            create_date: Utc.timestamp(1_592_739_359, 0),
+            gfy_id: String::from("enormousdescriptiveindri"),
+            gfy_name: String::from("EnormousDescriptiveIndri"),
+            gfy_number: 504_490_887,
+            gfy_slug: String::from("terrible-haircut-mistake-devine-mirror-color-wrong"),
+            has_audio: true,
+            has_transparency: false,
+            height: 600,
+            language_categories: to_string_vec(&["trending"]),
+            md5: String::from("86b4a541321b0985c4f30e2997a6594f"),
+            nsfw: Nsfw::Clean,
+            published: Published::Yes,
+            tags: to_string_vec(&[
+                "adam", "bad", "color", "devine", "dye", "first", "god", "hair", "haircut", "met",
+                "mirror", "mistake", "my", "oh", "omg", "oops", "terrible", "we", "when", "wrong",
+            ]),
+            title: String::from("OMG"),
+            user_data: User {
+                name: String::from("Deus GIF Machina"),
+                profile_image_url: String::from("https://profiles.gfycat.com/f78474b28ad6ace82bc702e4fdcdd12bb2a5416ecf62e73967811f63b3fcadc5.png"),
+                url: String::from("https://gfycat.com/@gifmachina"),
+                username: String::from("gifmachina"),
+                verified: false,
+            },
+            width: 940,
+        };
+
         let item = Client::default()?
             .get_item("enormousdescriptiveindri")
             .await?;
+        assert_eq!(item, expected_item);
 
-        let expected_tags = [
-            "adam", "bad", "color", "devine", "dye", "first", "god", "hair", "haircut", "met",
-            "mirror", "mistake", "my", "oh", "omg", "oops", "terrible", "we", "when", "wrong",
-        ];
-        assert_eq!(item.gfy_id, "enormousdescriptiveindri");
-        assert_eq!(item.gfy_name, "EnormousDescriptiveIndri");
-        assert_eq!(item.language_categories, ["trending"]);
-        assert_eq!(item.nsfw, Nsfw::Clean);
-        assert_eq!(item.published, Published::Yes);
-        assert_eq!(item.tags, expected_tags);
-        assert_eq!(item.title, "OMG");
         Ok(())
     }
 }
